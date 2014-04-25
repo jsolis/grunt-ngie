a02c875de8c861fc408bbadcff914ac39c38d8ff
Jason Solis <jsolis@gmail.com>
2014-04-25 17:52:19 -0400

    fixed test script

--- test/ngie_test.js
+++ test/ngie_test.js
@@ -30,8 +30,8 @@ exports.ngie = {
   default_options: function (test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
+    var actual = grunt.file.read('tmp/index-default.html');
+    var expected = grunt.file.read('test/expected/index-default.html');
     test.equal(actual, expected, 'should describe what the default behavior is.');
 
     test.done();
@@ -39,8 +39,8 @@ exports.ngie = {
   custom_options: function (test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
+    var actual = grunt.file.read('tmp/index-custom.html');
+    var expected = grunt.file.read('test/expected/index-custom.html');
     test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
 
     test.done();
